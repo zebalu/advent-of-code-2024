5c2de02fbbf8eb203d81b312f40fcec4e4192a76
zebalu <zebalu@gmail.com>
2024-12-08 07:22:35 +0100

    Day 08 it is still not clean, but at least I got rid of the grabage
    honestly: as long as I don't understand the task, I can not clean it better... :(

--- solution/src/main/java/io/github/zebalu/aoc2024/Day08.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day08.java
@@ -6,7 +6,6 @@ import java.util.*;
 
 public class Day08 extends AbstractDay {
     private final Map<Character, List<Coord>> antennaMap = new HashMap<>();
-    private final Set<Coord> antennaLocations = new HashSet<>();
     private final int height;
     private final int width;
     public Day08() {
@@ -24,38 +23,20 @@ public class Day08 extends AbstractDay {
                 if(c !='.') {
                     Coord coord = new Coord(x,y);
                     antennaMap.computeIfAbsent(c, _ -> new ArrayList<>()).add(coord);
-                    antennaLocations.add(coord);
                 }
             }
         }
     }
     @Override
     public String part1() {
-        System.out.println(antennaMap);
-        Set<Coord> antinoeds = new HashSet<>();
-        int count = 0;
+        Set<Coord> antinodes = new HashSet<>();
         for(var v: antennaMap.values()) {
-            var a = countAntinodesFor(v);
-            count += a.size();
-            antinoeds.addAll(a);
+            antinodes.addAll(collectCloseAntinodesFor(v));
         }
-        var lines = INPUT.lines().toList();
-        for(int y=0; y<height; y++) {
-            var line = lines.get(y);
-            for(int x=0; x<width; x++) {
-                Coord c = new Coord(x,y);
-                if(antinoeds.contains(c)) {
-                    System.out.print('#');
-                } else {
-                    System.out.print(line.charAt(x));
-                }
-            }
-            System.out.println();
-        }
-        return ""+antinoeds.size()+"\t"+count;
+        return Integer.toString(antinodes.size());
     }
 
-    private Set<Coord> countAntinodesFor(List<Coord> coords) {
+    private Set<Coord> collectCloseAntinodesFor(List<Coord> coords) {
         Set<Coord> forbidden = new HashSet<>(coords);
         Set<Coord> collector = new HashSet<>();
         for(int i=0; i<coords.size(); i++) {
@@ -68,7 +49,7 @@ public class Day08 extends AbstractDay {
                 c2.add(c1.minus(c2)),
                 c2.add(c2.minus(c1)));
                 for(Coord o : options) {
-                    if(!coords.contains(o) && isValid(o)) {
+                    if(!forbidden.contains(o) && isValid(o)) {
                         collector.add(o);
                     }
                 }
@@ -77,31 +58,22 @@ public class Day08 extends AbstractDay {
         return collector;
     }
 
-    private Set<Coord> collectAntinodes(List<Coord> coords) {
-        Set<Coord> forbidden = new HashSet<>(); //coords);
+    private Set<Coord> collectAllAntinodes(List<Coord> coords) {
         Set<Coord> collector = new HashSet<>();
         for(int i=0; i<coords.size(); i++) {
             for(int j=i+1; j<coords.size(); j++) {
                 Coord c1 = coords.get(i);
                 Coord c2 = coords.get(j);
-                var options = List.of(
-                        c1.add(c2.minus(c1)),
-                        c1.add(c1.minus(c2)),
-                        c2.add(c1.minus(c2)),
-                        c2.add(c2.minus(c1)));
-                var coll = new ArrayList<Coord>();
+                var coll = new HashSet<Coord>();
                 Coord dif = c2.minus(c1);
                 var next = c1;
                 while (isValid(next)) {
-                    if(!forbidden.contains(next)) {
-                        coll.add(next);
-                    }
+                    coll.add(next);
                     next = next.add(dif);
-                }next = c1;
+                }
+                next = c1;
                 while (isValid(next)) {
-                    if(!forbidden.contains(next)) {
-                        coll.add(next);
-                    }
+                    coll.add(next);
                     next = next.minus(dif);
                 }
                 collector.addAll(coll);
@@ -117,26 +89,10 @@ public class Day08 extends AbstractDay {
     @Override
     public String part2() {
         Set<Coord> antinodes = new HashSet<>();
-        int count = 0;
         for(var v: antennaMap.values()) {
-            var a = collectAntinodes(v);
-            count += a.size();
-            antinodes.addAll(a);
+            antinodes.addAll(collectAllAntinodes(v));
         }
-        var lines = INPUT.lines().toList();
-        for(int y=0; y<height; y++) {
-            var line = lines.get(y);
-            for(int x=0; x<width; x++) {
-                Coord c = new Coord(x,y);
-                if(antinodes.contains(c)) {
-                    System.out.print('#');
-                } else {
-                    System.out.print(line.charAt(x));
-                }
-            }
-            System.out.println();
-        }
-        return ""+antinodes.size()+"\t"+count;
+        return Integer.toString(antinodes.size());
     }
 
     private record Coord(int x, int y) {
@@ -150,45 +106,9 @@ public class Day08 extends AbstractDay {
 
     public static void main(String[] args) {
         String input = IOUtil.readInput(8);
-        Day08 day08 = new Day08(input);//EXAMPLE3);//EXAMPLE);//EXAMPLE3);//input);//EXAMPLE);//input);//EXAMPLE);
+        Day08 day08 = new Day08(input);
         System.out.println(day08.part1());
         System.out.println(day08.part2());
     }
-
-    private static final String EXAMPLE = """
-            ............
-            ........0...
-            .....0......
-            .......0....
-            ....0.......
-            ......A.....
-            ............
-            ............
-            ........A...
-            .........A..
-            ............
-            ............""";
-    private static final String EXAMPLE2= """
-            ..........
-            ..........
-            ..........
-            ....a.....
-            ..........
-            .....a....
-            ..........
-            ..........
-            ..........
-            ..........""";
-    private static final String EXAMPLE3= """
-            T.........
-            ...T......
-            .T........
-            ..........
-            ..........
-            ..........
-            ..........
-            ..........
-            ..........
-            ..........""";
 }
 
