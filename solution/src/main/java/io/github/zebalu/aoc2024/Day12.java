ee385cd433c63ed55f61338b723b377618f83428
zebalu <zebalu@gmail.com>
2024-12-12 07:35:41 +0100

    Day12 after refactor

--- solution/src/main/java/io/github/zebalu/aoc2024/Day12.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day12.java
@@ -3,46 +3,52 @@ package io.github.zebalu.aoc2024;
 import io.github.zebalu.aoc2024.utils.IOUtil;
 
 import java.util.*;
+import java.util.function.UnaryOperator;
 
-public class Day12 extends AbstractDay{
+public class Day12 extends AbstractDay {
     private final int height;
     private final int width;
     private final char[][] data;
+    private final Set<Set<Coord>> plots;
 
     public Day12() {
         this(IOUtil.readInput(12));
     }
+
     Day12(String input) {
         super(input, "Garden Groups", 12);
         data = IOUtil.readCharGrid(INPUT);
         height = data.length;
         width = data[0].length;
+        plots = new HashSet<>();
+        Set<Coord> processed = new HashSet<>();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Coord current = new Coord(x, y);
+                if (!processed.contains(current)) {
+                    Set<Coord> plot = findPlot(current);
+                    processed.addAll(plot);
+                    plots.add(plot);
+                }
+            }
+        }
     }
 
     @Override
     public String part1() {
-        long sum =0L;
-        Set<Coord> processed = new HashSet<>();
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                Coord current = new Coord(x,y);
-                if(!processed.contains(current)) {
-                    Set<Coord> plot = findPlot(current);
-                    processed.addAll(plot);
-                    long area = areaOf(plot);
-                    long perimiter = perimiterOf(plot);
-                    sum += area*perimiter;
-                }
-            }
-        }
-        return ""+sum;
+        long sum = plots.stream().mapToLong(plot -> {
+            long area = areaOf(plot);
+            long perimeter = perimeterOf(plot);
+            return area * perimeter;
+        }).sum();
+        return Long.toString(sum);
     }
 
-    private long perimiterOf(Set<Coord> plot) {
+    private long perimeterOf(Set<Coord> plot) {
         long count = 0;
-        for(Coord current : plot) {
-            for(Coord neighbour: current.neighbours()) {
-                if(!plot.contains(neighbour)) {
+        for (Coord current : plot) {
+            for (Coord neighbour : current.neighbours()) {
+                if (!plot.contains(neighbour)) {
                     ++count;
                 }
             }
@@ -62,12 +68,12 @@ public class Day12 extends AbstractDay{
         plot.add(start);
         seen.add(start);
         queue.add(start);
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             Coord current = queue.poll();
             current.neighbours().stream()
                     .filter(seen::add)
                     .filter(this::isValid)
-                    .filter(c->getType(c)==type)
+                    .filter(c -> getType(c) == type)
                     .forEach(neighbour -> {
                         plot.add(neighbour);
                         queue.add(neighbour);
@@ -86,134 +92,71 @@ public class Day12 extends AbstractDay{
 
     @Override
     public String part2() {
-        long sum =0L;
-        Set<Coord> processed = new HashSet<>();
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                Coord current = new Coord(x,y);
-                if(!processed.contains(current)) {
-                    Set<Coord> plot = findPlot(current);
-                    processed.addAll(plot);
-                    long area = areaOf(plot);
-                    long sides = sidesOf(plot);
-                    sum += area*sides;
-                }
-            }
-        }
-        return ""+sum;
+        long sum = plots.stream().mapToLong(plot->{
+            long area = areaOf(plot);
+            long sides = sidesOf(plot);
+            return area * sides;
+        }).sum();
+        return Long.toString(sum);
     }
 
     private long sidesOf(Set<Coord> plot) {
-        Set<Coord> outerSpots = new HashSet<>();
         Set<Set<Border>> edges = new HashSet<>();
-        for(Coord current : plot) {
-            List<Coord> neighbours = current.neighbours();;
-            Iterator<Coord> ni = neighbours.iterator();
-            boolean foundMissing = false;
-            while(ni.hasNext() && !foundMissing) {
-                Coord next = ni.next();
-                foundMissing = !plot.contains(next);
-            }
-            if(!foundMissing) {
-                outerSpots.add(current);
-            }
-        }
         for (Coord spot : plot) {
-            edges.addAll(verticalEdgesOf(spot, plot));
-            edges.addAll(horizontalEdgesOf(spot, plot));
+            edges.addAll(edgesOf(spot, plot, Coord.horizontal, Coord.vertical, edges));
+            edges.addAll(edgesOf(spot, plot, Coord.vertical, Coord.horizontal, edges));
         }
         return edges.size();
     }
 
-    private Set<Set<Border>> horizontalEdgesOf(Coord spot, Set<Coord> plot) {
+    private Set<Set<Border>> edgesOf(Coord spot, Set<Coord> plot, List<UnaryOperator<Coord>> walks, List<UnaryOperator<Coord>> checks, Set<Set<Border>> foundEdges) {
         Set<Set<Border>> edges = new HashSet<>();
-        Coord current = spot.above();
-        Set<Border> edge = new HashSet<>();
-        if(!plot.contains(current)) {
-            edge.add(new Border(spot, current));
-            Coord walk = current.left();
-            while(plot.contains(walk.below())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.below(), walk));
-                walk = walk.left();
+        for (UnaryOperator<Coord> innerCheck : checks) {
+            UnaryOperator<Coord> outerCheck = innerCheck == checks.getFirst() ? checks.getLast() : checks.getFirst();
+            Coord current = outerCheck.apply(spot);
+            Set<Border> edge = new HashSet<>();
+            Border start = new Border(spot, current);
+            if (!plot.contains(current) && !alreadyProcessed(start, foundEdges)) {
+                edge.add(start);
+                for (UnaryOperator<Coord> walker : walks) {
+                    Coord walk = walker.apply(current);
+                    while (plot.contains(innerCheck.apply(walk)) && !plot.contains(walk)) {
+                        edge.add(new Border(innerCheck.apply(walk), walk));
+                        walk = walker.apply(walk);
+                    }
+                }
+                edges.add(edge);
             }
-            walk = current.right();
-            while(plot.contains(walk.below())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.below(), walk));
-                walk = walk.right();
-            }
-            edges.add(edge);
-        }
-        current = spot.below();
-        edge = new HashSet<>();
-        if(!plot.contains(current)) {
-            edge.add(new Border(spot, current));
-            Coord walk = current.left();
-            while(plot.contains(walk.above())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.above(), walk));
-                walk = walk.left();
-            }
-            walk = current.right();
-            while(plot.contains(walk.above())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.above(), walk));
-                walk = walk.right();
-            }
-            edges.add(edge);
         }
         return edges;
     }
 
-    private Set<Set<Border>> verticalEdgesOf(Coord spot, Set<Coord> plot) {
-        Set<Set<Border>> edges = new HashSet<>();
-        Coord current = spot.left();
-        Set<Border> edge = new HashSet<>();
-        if(!plot.contains(current)) {
-            edge.add(new Border(spot, current));
-            Coord walk = current.below();
-            while(plot.contains(walk.right())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.right(), walk));
-                walk = walk.below();
-            }
-            walk = current.above();
-            while(plot.contains(walk.right())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.right(), walk));
-                walk = walk.above();
-            }
-            edges.add(edge);
-        }
-        current = spot.right();
-        edge = new HashSet<>();
-        if(!plot.contains(current)) {
-            edge.add(new Border(spot, current));
-            Coord walk = current.below();
-            while(plot.contains(walk.left())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.left(), walk));
-                walk = walk.below();
-            }
-            walk = current.above();
-            while(plot.contains(walk.left())&&!plot.contains(walk)) {
-                edge.add(new Border(walk.left(), walk));
-                walk = walk.above();
-            }
-            edges.add(edge);
-        }
-        return edges;
+    private boolean alreadyProcessed(Border start, Set<Set<Border>> foundEdges) {
+        return foundEdges.stream().anyMatch(edge -> edge.contains(start));
     }
 
     private record Coord(int x, int y) {
+        final static List<UnaryOperator<Coord>> horizontal = List.of(Coord::left, Coord::right);
+        final static List<UnaryOperator<Coord>> vertical = List.of(Coord::above, Coord::below);
+
         List<Coord> neighbours() {
-            return List.of(new Coord(x-1, y), new Coord(x+1, y), new Coord(x, y-1), new Coord(x, y+1));
+            return List.of(new Coord(x - 1, y), new Coord(x + 1, y), new Coord(x, y - 1), new Coord(x, y + 1));
         }
+
         Coord above() {
-            return new Coord(x, y-1);
+            return new Coord(x, y - 1);
         }
+
         Coord below() {
-            return new Coord(x, y+1);
+            return new Coord(x, y + 1);
         }
+
         Coord left() {
-            return new Coord(x-1, y);
+            return new Coord(x - 1, y);
         }
+
         Coord right() {
-            return new Coord(x+1, y);
+            return new Coord(x + 1, y);
         }
     }
 
