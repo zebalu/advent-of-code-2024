49e613f5617a4be3f234659d27595f575d93d11e
zebalu <zebalu@gmail.com>
2024-12-14 07:00:26 +0100

    Day14 cleanup

--- solution/src/main/java/io/github/zebalu/aoc2024/Day14.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day14.java
@@ -5,9 +5,10 @@ import io.github.zebalu.aoc2024.utils.IOUtil;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day14 extends AbstractDay {
+    private static final int WIDTH = 101;
+    private static final int HEIGHT = 103;
     public Day14() {
         this(IOUtil.readInput(14));
     }
@@ -18,38 +19,36 @@ public class Day14 extends AbstractDay {
 
     @Override
     public String part1() {
-        int width = 101;
-        int height = 103;
-        List<Robot> robots = INPUT.lines().map(l->Robot.fromString(l, width, height)).toList();
+        List<Robot> robots = INPUT.lines().map(Robot::fromString).toList();
         for(int i=0; i<100; ++i) {
             robots.forEach(Robot::move);
         }
-        int q0 = 0;
         int q1 = 0;
         int q2 = 0;
         int q3 = 0;
         int q4 = 0;
         for(Robot robot: robots) {
             switch (robot.getQuadrant()) {
-                case 0 -> q0++;
                 case 1 -> q1++;
                 case 2 -> q2++;
                 case 3 -> q3++;
                 case 4 -> q4++;
-                default -> throw new IllegalStateException("Can not be in " + robot.getQuadrant());
             }
         }
         int safetyScore = q1*q2*q3*q4;
-        return ""+safetyScore;
+        return Integer.toString(safetyScore);
     }
 
     @Override
     public String part2() {
-        int width = 101;
-        int height = 103;
-        List<Robot> robots = INPUT.lines().map(l->Robot.fromString(l, width, height)).toList();
-        boolean noCollisionDetected = robots.stream().map(Coord::fromRobot).collect(Collectors.toSet()).size() == robots.size();
+        record Coord(int x, int y) {
+            static Coord fromRobot(Robot robot) {
+                return new Coord(robot.x, robot.y);
+            }
+        }
+        List<Robot> robots = INPUT.lines().map(Robot::fromString).toList();
         int stepcount = 0;
+        boolean noCollisionDetected;
         do {
             ++stepcount;
             Set<Coord> positions = new HashSet<>();
@@ -59,40 +58,7 @@ public class Day14 extends AbstractDay {
             }
             noCollisionDetected = robots.size() == positions.size();
         } while (!noCollisionDetected);
-        print(robots);
-        return ""+stepcount;
-    }
-    private void print(List<Robot> robots) {
-        record Coord(int x, int y) {}
-        Map<Coord, Robot> map = new HashMap<>();
-        robots.forEach(robot->{
-            map.put(new Coord(robot.x, robot.y), robot);
-        });
-        System.out.println("-".repeat(101));
-        for(int y=0; y<103; ++y) {
-            for(int x=0; x<101; ++x) {
-                Coord coord = new Coord(x, y);
-                if(map.containsKey(coord)) {
-                    System.out.print('#');
-                } else {
-                    System.out.print('.');
-                }
-            }
-            System.out.println();
-        }
-        System.out.println("-".repeat(101));
-    }
-
-    private record Coord(int x, int y) {
-        List<Record> neighbours() {
-            return List.of(new Coord(x-1, y), new Coord(x+1, y), new Coord(x, y+1), new Coord(x, y-1));
-        }
-        static Coord fromRobot(Robot robot) {
-            return new Coord(robot.x, robot.y);
-        }
-        int countNeighbours(Set<Coord> neighbours) {
-            return (int)neighbours().stream().filter(neighbours::contains).count();
-        }
+        return Integer.toString(stepcount);
     }
 
     private static class Robot {
@@ -116,10 +82,8 @@ public class Day14 extends AbstractDay {
         }
 
         void move() {
-            //System.out.print(String.format("((%3d, %3d), (%3d,%3d)) --> ", x,y,vx,vy));
             x = (x + vx + width) % width;
             y = (y + vy + height) % height;
-            //System.out.println(String.format("((%3d, %3d), (%3d,%3d))", x,y,vx,vy));
         }
 
         int getQuadrant() {
@@ -139,14 +103,14 @@ public class Day14 extends AbstractDay {
             return 0;
         }
 
-        static Robot fromString(String line, int width, int height) {
+        static Robot fromString(String line) {
             Matcher matcher = numberpattern.matcher(line);
             if (matcher.find()) {
                 int x = Integer.parseInt(matcher.group(1));
                 int y = Integer.parseInt(matcher.group(2));
                 int vx = Integer.parseInt(matcher.group(3));
                 int vy = Integer.parseInt(matcher.group(4));
-                return new Robot(x, y, vx, vy, width, height);
+                return new Robot(x, y, vx, vy, WIDTH, HEIGHT);
             }
             throw new IllegalArgumentException("Can read line: " + line);
         }
