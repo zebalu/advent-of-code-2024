4d386119e6bd784e26afcdc7d5c9d40ecb4c9bc2
zebalu <zebalu@gmail.com>
2024-12-16 07:05:24 +0100

    Day16 clean

--- solution/src/main/java/io/github/zebalu/aoc2024/Day16.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day16.java
@@ -5,88 +5,78 @@ import io.github.zebalu.aoc2024.utils.IOUtil;
 import java.util.*;
 
 public class Day16 extends AbstractDay {
-    char[][] grid;
-    int height;
-    int width;
-    Coord start;
-    Coord end;
+    private final char[][] grid;
+    private final int height;
+    private final int width;
+    private final Coord start;
+    private final Coord end;
+    private final Set<Coord> bestPathPoints = new HashSet<>();
+    private final Map<Raindeer, Integer> seen = new HashMap<>();
+
+    private int minPrice = Integer.MAX_VALUE;
+
     public Day16() {
         this(IOUtil.readInput(16));
     }
+
     public Day16(String input) {
-        super(input, "?????", 16);
+        super(input, "Reindeer Maze", 16);
         grid = IOUtil.readCharGrid(INPUT);
         height = grid.length;
         width = grid[0].length;
-        for(int y=0; y<height; y++) {
-            for(int x = 0; x < width; x++) {
+        Coord s = null;
+        Coord e = null;
+        for (int y = 0; y < height & (s == null || e == null); y++) {
+            for (int x = 0; x < width & (s == null || e == null); x++) {
                 if (grid[y][x] == 'S') {
-                    start = new Coord(x, y);
+                    s = new Coord(x, y);
                 } else if (grid[y][x] == 'E') {
-                    end = new Coord(x, y);
+                    e = new Coord(x, y);
                 }
             }
         }
+        start = s;
+        end = e;
+        findPathes();
     }
 
     @Override
     public String part1() {
-        //Set<Raindeer> seen = new HashSet<>();
-        Set<Coord> pathPoints = new HashSet<>();
-        Map<Raindeer, Integer> seen = new HashMap<>();
+        return Integer.toString(minPrice);
+    }
+
+    @Override
+    public String part2() {
+        return Integer.toString(bestPathPoints.size());
+    }
+
+    private void findPathes() {
         Queue<State> priorityQueue = new PriorityQueue<>(State.PRICE_COMPARATOR);
-        //seen.add(new Raindeer(start, new Coord(1, 0)));
-        seen.put(new Raindeer(start, new Coord(1,0)), 0);
+        seen.put(new Raindeer(start, new Coord(1, 0)), 0);
         priorityQueue.add(new State(new Raindeer(start, new Coord(1, 0)), 0, new HashSet<>(List.of(start))));
-        int price = Integer.MAX_VALUE;
-        int count = Integer.MIN_VALUE;
         boolean weAreOverBestPathes = false;
-        while(!priorityQueue.isEmpty() && !weAreOverBestPathes) {
+        while (!priorityQueue.isEmpty() && !weAreOverBestPathes) {
             State state = priorityQueue.poll();
-            for(State next: state.next()) {
-                if(isFree(next.raindeer.position) && seen.getOrDefault(next.raindeer, Integer.MAX_VALUE) >= next.price) {
+            for (State next : state.next()) {
+                if (isFree(next.raindeer.position) && next.price <= seen.getOrDefault(next.raindeer, Integer.MAX_VALUE)) {
                     priorityQueue.add(next);
                     seen.put(next.raindeer, next.price);
-                    if(end.equals(next.raindeer.position)) {
-                        if(next.price>price) {
+                    if (end.equals(next.raindeer.position)) {
+                        if (minPrice < next.price) {
                             weAreOverBestPathes = true;
                         }
-                        price = Math.min(price, next.price);
-                        pathPoints.addAll(next.visited);
+                        minPrice = Math.min(minPrice, next.price);
+                        bestPathPoints.addAll(next.visited);
                     }
                 }
             }
-            //System.out.println(priorityQueue.size());
         }
-        System.out.println(priorityQueue.peek());
-        System.out.println("copunt: "+pathPoints.size());
-        /*
-        State s = priorityQueue.poll();
-        for(int y=0; y<height; y++) {
-            for (int x = 0; x < width; x++) {
-                Coord c=new Coord(x,y);
-                if(!s.visited.contains(c)) {
-                    System.out.print(grid[y][x]);
-                } else {
-                    System.out.print("O");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println(s.visited.size());
-
-         */
-        return ""+price;
     }
 
     private boolean isFree(Coord position) {
         return position.x >= 0 && position.x < width && position.y >= 0 && position.y < height && grid[position.y][position.x] != '#';
     }
 
-    @Override
-    public String part2() {
-        return "";
-    }
 
     private record Coord(int x, int y) {
         Coord add(Coord other) {
@@ -98,27 +88,34 @@ public class Day16 extends AbstractDay {
         Raindeer turnLeft() {
             return new Raindeer(position, new Coord(-orientation.y, orientation.x));
         }
+
         Raindeer turnRight() {
             return new Raindeer(position, new Coord(orientation.y, -orientation.x));
         }
+
         Raindeer step() {
             return new Raindeer(position.add(orientation), orientation);
         }
     }
+
     private record State(Raindeer raindeer, int price, Set<Coord> visited) {
         static Comparator<State> PRICE_COMPARATOR = Comparator.comparing(State::price);
+
         State turnLeft() {
-            return new State(raindeer.turnLeft(), price+1000, visited);
+            return new State(raindeer.turnLeft(), price + 1000, visited);
         }
+
         State turnRight() {
-            return new State(raindeer.turnRight(), price+1000, visited);
+            return new State(raindeer.turnRight(), price + 1000, visited);
         }
+
         State step() {
             Raindeer step = raindeer.step();
             var v = new HashSet<>(visited);
             v.add(step.position);
-            return new State(step, price+1, v);
+            return new State(step, price + 1, v);
         }
+
         List<State> next() {
             return List.of(turnLeft(), turnRight(), step());
         }
@@ -126,43 +123,8 @@ public class Day16 extends AbstractDay {
 
     public static void main(String[] args) {
         String input = IOUtil.readInput(16);
-        Day16 day16 = new Day16(input);//EXAMPLE_2);//EXAMPLE_1);//input);//EXAMPLE_2);//EXAMPLE_1);//input);
+        Day16 day16 = new Day16(input);
         System.out.println(day16.part1());
         System.out.println(day16.part2());
     }
-
-    private static final String EXAMPLE_1= """
-            ###############
-            #.......#....E#
-            #.#.###.#.###.#
-            #.....#.#...#.#
-            #.###.#####.#.#
-            #.#.#.......#.#
-            #.#.#####.###.#
-            #...........#.#
-            ###.#.#####.#.#
-            #...#.....#.#.#
-            #.#.#.###.#.#.#
-            #.....#...#.#.#
-            #.###.#.#.#.#.#
-            #S..#.....#...#
-            ###############""";
-    private static final String EXAMPLE_2= """
-            #################
-            #...#...#...#..E#
-            #.#.#.#.#.#.#.#.#
-            #.#.#.#...#...#.#
-            #.#.#.#.###.#.#.#
-            #...#.#.#.....#.#
-            #.#.#.#.#.#####.#
-            #.#...#.#.#.....#
-            #.#.#####.#.###.#
-            #.#.#.......#...#
-            #.#.###.#####.###
-            #.#.#...#.....#.#
-            #.#.#.#####.###.#
-            #.#.#.........#.#
-            #.#.#.#########.#
-            #S#.............#
-            #################""";
 }
