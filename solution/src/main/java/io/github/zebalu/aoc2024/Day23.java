b03957ed7f20fd43b3f521ee577aeafe41d08031
zebalu <zebalu@gmail.com>
2024-12-23 07:08:05 +0100

    Day23 format file

--- solution/src/main/java/io/github/zebalu/aoc2024/Day23.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day23.java
@@ -7,9 +7,11 @@ import java.util.stream.Collectors;
 
 public class Day23 extends AbstractDay {
     private final Map<String, Set<String>> network;
+
     public Day23() {
         this(IOUtil.readInput(23));
     }
+
     public Day23(String input) {
         super(input, "LAN Party", 23);
         network = new HashMap<>();
@@ -18,63 +20,6 @@ public class Day23 extends AbstractDay {
             saveConnection(computers[0], computers[1]);
             saveConnection(computers[1], computers[0]);
         });
-    }
-
-    private void saveConnection(String from, String to) {
-        network.computeIfAbsent(from, _ -> new HashSet<>()).add(to);
-    }
-
-    private Set<Set<String>> interconnectedTrios() {
-        Set<Set<String>> interconnectedTrios = new HashSet<>();
-        for(String firstNode : network.keySet()) {
-            Set<String> connections = network.get(firstNode);
-            for (String secondNode : connections) {
-                if(!secondNode.equals(firstNode)) {
-                    for(String thirdNode : network.get(secondNode)) {
-                        if(!thirdNode.equals(firstNode)) {
-                            Set<String> thirdConnections = network.get(thirdNode);
-                            if(thirdConnections.contains(secondNode) && thirdConnections.contains(firstNode)) {
-                                interconnectedTrios.add(Set.of(firstNode, secondNode, thirdNode));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return interconnectedTrios;
-    }
-
-    private Set<Set<String>> findGroups() {
-        Set<Set<String>> groups = new HashSet<>();
-        for(String firstNode : network.keySet()) {
-            Set<String> passed = new HashSet<>();
-            Set<String> checked = new HashSet<>();
-            Queue<String> queue = new ArrayDeque<>();
-            queue.add(firstNode);
-            while(!queue.isEmpty()) {
-                String node = queue.poll();
-                if(!checked.contains(node) && network.get(node).containsAll(passed)) {
-                    passed.add(node);
-                    checked.add(node);
-                    for(String next : network.get(node)) {
-                        if(!checked.contains(next)) {
-                            queue.add(next);
-                        }
-                    }
-                }
-            }
-            groups.add(passed);
-        }
-        return groups;
-    }
-
-    public boolean anyComputerStartsWithT(Set<String> computers) {
-        for(String computer : computers) {
-            if(computer.startsWith("t")) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
@@ -87,6 +32,63 @@ public class Day23 extends AbstractDay {
     public String part2() {
         Set<String> largest = findGroups().stream().max(Comparator.comparingInt(Set::size)).orElseThrow();
         return largest.stream().sorted().collect(Collectors.joining(","));
+    }
+
+    private void saveConnection(String from, String to) {
+        network.computeIfAbsent(from, _ -> new HashSet<>()).add(to);
+    }
+
+    private Set<Set<String>> interconnectedTrios() {
+        Set<Set<String>> interconnectedTrios = new HashSet<>();
+        for (String firstNode : network.keySet()) {
+            Set<String> connections = network.get(firstNode);
+            for (String secondNode : connections) {
+                if (!secondNode.equals(firstNode)) {
+                    for (String thirdNode : network.get(secondNode)) {
+                        if (!thirdNode.equals(firstNode)) {
+                            Set<String> thirdConnections = network.get(thirdNode);
+                            if (thirdConnections.contains(secondNode) && thirdConnections.contains(firstNode)) {
+                                interconnectedTrios.add(Set.of(firstNode, secondNode, thirdNode));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return interconnectedTrios;
+    }
+
+    private Set<Set<String>> findGroups() {
+        Set<Set<String>> groups = new HashSet<>();
+        for (String firstNode : network.keySet()) {
+            Set<String> passed = new HashSet<>();
+            Set<String> checked = new HashSet<>();
+            Queue<String> queue = new ArrayDeque<>();
+            queue.add(firstNode);
+            while (!queue.isEmpty()) {
+                String node = queue.poll();
+                if (!checked.contains(node) && network.get(node).containsAll(passed)) {
+                    passed.add(node);
+                    checked.add(node);
+                    for (String next : network.get(node)) {
+                        if (!checked.contains(next)) {
+                            queue.add(next);
+                        }
+                    }
+                }
+            }
+            groups.add(passed);
+        }
+        return groups;
+    }
+
+    private boolean anyComputerStartsWithT(Set<String> computers) {
+        for (String computer : computers) {
+            if (computer.startsWith("t")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
