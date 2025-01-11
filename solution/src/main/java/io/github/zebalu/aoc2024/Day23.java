30ee4bd2c2b1feff0b5830dce8b13aaf507424b9
Balázs Zaicsek <zebalu@gmail.com>
2025-01-11 17:27:52 +0100

    change to Bron-Kerbosch Clique find algorithm

--- solution/src/main/java/io/github/zebalu/aoc2024/Day23.java
+++ solution/src/main/java/io/github/zebalu/aoc2024/Day23.java
@@ -30,7 +30,7 @@ public class Day23 extends AbstractDay {
 
     @Override
     public String part2() {
-        Set<String> largest = findGroups().stream().max(Comparator.comparingInt(Set::size)).orElseThrow();
+        Set<String> largest = bronKerboschCliquesFind().stream().max(Comparator.comparingInt(Set::size)).orElseThrow();
         return largest.stream().sorted().collect(Collectors.joining(","));
     }
 
@@ -41,38 +41,50 @@ public class Day23 extends AbstractDay {
     private Set<Set<String>> interconnectedTriosWithStartingT() {
         Set<Set<String>> interconnectedTrios = new HashSet<>();
         network.keySet().stream().filter(s -> s.startsWith("t"))
-                .forEach(firstNode -> network.get(firstNode).stream().filter(secondNode -> !secondNode.equals(firstNode))
-                        .forEach(secondNode -> network.get(secondNode).stream().filter(thirdNode -> !thirdNode.equals(firstNode) && network.get(thirdNode).contains(firstNode))
+                .forEach(firstNode -> network.get(firstNode).stream()
+                        .forEach(secondNode -> network.get(firstNode).stream().filter(thirdNode -> !thirdNode.equals(secondNode) && network.get(thirdNode).contains(secondNode))
                                 .forEach(thirdNode -> interconnectedTrios.add(Set.of(firstNode, secondNode, thirdNode)))));
         return interconnectedTrios;
     }
 
-    private List<Set<String>> findGroups() {
-        Set<String> processed = new HashSet<>();
+    private List<Set<String>> bronKerboschCliquesFind() {
+        record State(Set<String> clique, Set<String> candidates, Set<String> excludes) {
+
+            boolean isFinalClique() {
+                return candidates.isEmpty() && excludes.isEmpty();
+            }
+
+            State extend(String c, Set<String> neighbours, Set<String> remainingCandidates, Set<String> extendedExclude) {
+                return new State(extend(clique, c), intersect(neighbours, remainingCandidates), intersect(neighbours, extendedExclude));
+            }
+
+            static Set<String> intersect(Set<String> a, Set<String> b) {
+                HashSet<String> cpy = new HashSet<>(a.size() < b.size() ? a : b);
+                cpy.retainAll(a.size() < b.size() ? b : a);
+                return cpy;
+            }
+
+            static Set<String> extend(Set<String> a, String b) {
+                Set<String> cpy = new HashSet<>(a);
+                cpy.add(b);
+                return cpy;
+            }
+        }
         List<Set<String>> groups = new ArrayList<>();
-        for (String firstNode : network.keySet()) {
-            Set<Set<String>> seen = new HashSet<>();
-            Queue<SequencedSet<String>> queue = new ArrayDeque<>();
-            queue.add(new LinkedHashSet<>(Set.of(firstNode)));
-            seen.add(queue.peek());
-            while (!queue.isEmpty()) {
-                SequencedSet<String> nodes = queue.poll();
-                boolean extended = false;
-                for (String connected : network.get(nodes.getLast())) {
-                    if(!processed.contains(connected) && network.get(connected).containsAll(nodes)) {
-                        SequencedSet<String> next = new LinkedHashSet<>(nodes);
-                        next.add(connected);
-                        if(seen.add(next)) {
-                            queue.add(next);
-                            extended = true;
-                        }
-                    }
-                }
-                if(!extended && nodes.size()>1) {
-                    groups.add(nodes);
+        Queue<State> queue = new ArrayDeque<>(List.of(new State(Set.of(), network.keySet(), Set.of())));
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+            if (state.isFinalClique()) {
+                groups.add(state.clique);
+            } else {
+                Set<String> remainingCandidates = new HashSet<>(state.candidates);
+                Set<String> extendedExclude = new HashSet<>(state.excludes);
+                for (String c : state.candidates) {
+                    queue.add(state.extend(c, network.get(c), remainingCandidates, extendedExclude));
+                    remainingCandidates.remove(c);
+                    extendedExclude.add(c);
                 }
             }
-            processed.add(firstNode);
         }
         return groups;
     }
